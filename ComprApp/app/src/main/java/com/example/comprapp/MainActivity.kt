f57e4cc2742d4fb604e7f53607fdb23c9aa5f853
Alex Olivares <a.olivaresdelrey.pierres@gmail.com>
2022-03-27 18:13:53 +0200

    Se solucionan algunos problemas
    Se cambia StartActivity por StartActivityForResult

--- ComprApp/app/src/main/java/com/example/comprapp/MainActivity.kt
+++ ComprApp/app/src/main/java/com/example/comprapp/MainActivity.kt
@@ -4,18 +4,37 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import com.example.comprapp.databinding.ActivityCameraBinding
 import com.example.comprapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var viewBinding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        viewBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(viewBinding.root)
 
         val open_button = findViewById<Button>(R.id.button_camera)
         open_button.setOnClickListener{
             val intent = Intent(this, Camera::class.java)
-            startActivity(intent)
+            startActivityForResult(intent, REQUEST_CAMERA_BARCODE)
         }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if(requestCode == REQUEST_CAMERA_BARCODE){
+            if(resultCode == RESULT_OK){
+                viewBinding.resultBarcode.text = data!!.dataString.toString()
+            } else if(resultCode == RESULT_CANCELED){
+                viewBinding.resultBarcode.text = data!!.dataString.toString()
+            }
+        }
+    }
+
+    companion object{
+        private const val REQUEST_CAMERA_BARCODE = 10
     }
 }
